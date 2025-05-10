4bb7c3bd1c909077bc1d7fd13062d610e36842dc
Alonza0314 <dufengplus@gmail.com>
2025-05-10 19:26:30 +0000

    fix: bson marshell error

--- backend/mongo/mongo.go
+++ backend/mongo/mongo.go
@@ -2,6 +2,7 @@ package mongo
 
 import (
 	"context"
+	"errors"
 
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
@@ -28,4 +29,46 @@ func GetOne(collectionName string, filter interface{}) (interface{}, error) {
 	}
 
 	return result, nil
+}
+
+func InsertOne(collectionName string, data interface{}) error {
+	client, err := mongo.Connect(options.Client().ApplyURI(HOST))
+	if err != nil {
+		return err
+	}
+
+	db := client.Database(DB)
+	collection := db.Collection(collectionName)
+	if collection == nil {
+		if err = db.CreateCollection(context.TODO(), collectionName); err != nil {
+			return err
+		}
+	}
+
+	_, err = collection.InsertOne(context.TODO(), data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func UpdateOne(collectionName string, filter interface{}, update interface{}) error {
+	client, err := mongo.Connect(options.Client().ApplyURI(HOST))
+	if err != nil {
+		return err
+	}
+
+	db := client.Database(DB)
+	collection := db.Collection(collectionName)
+	if collection == nil {
+		return errors.New("collection not found")
+	}
+
+	_, err = collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
